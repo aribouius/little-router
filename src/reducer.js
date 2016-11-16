4cf53fcf989d46343973dd880884a01821ec6904
Ari Bouius <abouius@rent.com>
2016-11-15 19:02:01 -0800

    update reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,22 +1,17 @@
-import { LOCATION_CHANGE, ROUTE_CHANGE } from './const'
+import { LOCATION_CHANGE } from './const'
 
 export const initialState = {
-  params: {},
-  transitioning: false,
-  location: {
-    href: null,
-    search: null,
-    pathname: null,
-    query: {},
-  },
+  href: null,
+  hash: null,
+  search: null,
+  pathname: null,
+  query: {},
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOCATION_CHANGE:
-      return { ...state, transitioning: true, location: { ...action.location } }
-    case ROUTE_CHANGE:
-      return { ...state, transitioning: false }
+      return { ...state, ...action.location }
     default:
       return state
   }
