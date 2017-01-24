3faf47c0f64f5190905a411e2af199e448bd8f7e
Ari Bouius <abouius@rent.com>
2017-01-24 13:32:55 -0800

    remove redux code

--- src/redux/const.js
+++
@@ -1,6 +0,0 @@
-export const POP = 'POP'
-export const PUSH = 'PUSH'
-export const REPLACE = 'REPLACE'
-
-export const GO = '@@router/GO'
-export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE'
--- src/redux/index.js
+++
@@ -1,4 +0,0 @@
-export reducer from './reducer'
-export historyMiddleware from './historyMiddleware'
-export { push, replace, go, goBack, goForward } from './actions'
-export { LOCATION_CHANGE } from './const'
