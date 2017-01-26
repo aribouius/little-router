ffd69336828bef5c5c47eba8a1afe09e1ca92b22
Ari Bouius <abouius@rent.com>
2017-01-26 08:20:03 -0800

    add negative match test

--- src/__tests__/resolve-test.js
+++ src/__tests__/resolve-test.js
@@ -5,6 +5,11 @@ describe('resolve', () => {
   const route1 = { foo: 'foo' }
   const route2 = { bar: 'bar' }
 
+  it('returns undefined when given empty array', () => {
+    const routes = []
+    expect(resolve(routes)).to.equal(undefined)
+  })
+
   it('returns the last matched route', () => {
     const routes = [route1, route2]
     expect(resolve(routes)).to.eql(route2)
